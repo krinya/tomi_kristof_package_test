eae3d1a35a58ae544f82044ef388a7040d30208f
Kristof Menyhert <menyhert.kristof@gmail.com>
2025-05-14 20:07:55 +0200

    Add initial README content to describe package purpose and functionality

--- src/tomi_kristof_package_test/__init__.py
+++ src/tomi_kristof_package_test/__init__.py
@@ -1,6 +1,6 @@
 # my_simple_package/my_simple_package/__init__.py
 # Make the version easily accessible (matches pyproject.toml)
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 from .core import greet
 __all__ = ['greet'] # Explicitly define the public API
